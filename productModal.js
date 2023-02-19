3195f387bd6b35762da1190fcc8d76ff27a230c9
tunafin <tunaweichi@gmail.com>
2023-02-19 18:02:16 +0800

    add components

---
+++ productModal.js
@@ -0,0 +1,31 @@
+export default {
+  template: '#productModalTemplate',
+  props: {
+    product: {
+      type: Object,
+      default() {
+        return {
+        }
+      }
+    }
+  },
+  data() {
+    return {
+      status: {},
+      modal: '',
+      qty: 1,
+    };
+  },
+  mounted() {
+    this.modal = new bootstrap.Modal(this.$refs.modal, {
+    });
+  },
+  methods: {
+    openModal() {
+      this.modal.show();
+    },
+    hideModal() {
+      this.modal.hide();
+    },
+  },
+}
